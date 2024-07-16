be1cd1106c2bd773d26f85310c6b7da7dea90a71
Bruce Manning <themoose3@gmail.com>
2024-07-16 17:54:40 -0400

    Prettified syntax

--- src/array_binary_search.rs
+++ src/array_binary_search.rs
@@ -1,7 +1,9 @@
-use std::cmp::Ordering::{ Equal, Less, Greater };
+use std::cmp::Ordering::{Equal, Greater, Less};
 
 #[allow(dead_code)]
-pub fn binary_search<T: Ord>(array: &[T], target: T) -> bool {
+pub fn binary_search<T: Ord, V: AsRef<[T]>>(array: V, target: T) -> bool {
+    let array: &[T] = array.as_ref();
+
     if array.is_empty() {
         return false;
     }
@@ -28,37 +30,37 @@ mod tests {
 
     #[test]
     fn edge_cases() {
-        assert!(binary_search(&[], 3).not());
-        assert!(binary_search(&[], -3).not());
-        assert!(binary_search(&[1], 3).not());
-        assert!(binary_search(&[1], -3).not());
-        assert!(binary_search(&[1], 1));
-        assert!(binary_search(&[1, 2], 2));
+        assert!(binary_search([], 3).not());
+        assert!(binary_search([], -3).not());
+        assert!(binary_search([1], 3).not());
+        assert!(binary_search([1], -3).not());
+        assert!(binary_search([1], 1));
+        assert!(binary_search([1, 2], 2));
     }
 
     #[test]
     fn contains_target() {
-        assert!(binary_search(&[1, 2, 3, 4, 5], 3));
-        assert!(binary_search(&[-1, 0, 2, 3, 4, 5], 3));
-        assert!(binary_search(&[-1, 0, 2, 3, 4, 5], -1));
-        assert!(binary_search(&[-1, 0, 2, 3, 4, 5], 5));
+        assert!(binary_search([1, 2, 3, 4, 5], 3));
+        assert!(binary_search([-1, 0, 2, 3, 4, 5], 3));
+        assert!(binary_search([-1, 0, 2, 3, 4, 5], -1));
+        assert!(binary_search([-1, 0, 2, 3, 4, 5], 5));
     }
 
     #[test]
     fn excludes_target() {
-        assert!(binary_search(&[1, 2, 3, 4, 5], 6).not());
-        assert!(binary_search(&[1, 2, 3, 4, 5], 0).not());
-        assert!(binary_search(&[-5, -4, -3, -2, -1], 6).not());
-        assert!(binary_search(&[-5, -4, -3, -2, -1], -6).not());
+        assert!(binary_search([1, 2, 3, 4, 5], 6).not());
+        assert!(binary_search([1, 2, 3, 4, 5], 0).not());
+        assert!(binary_search([-5, -4, -3, -2, -1], 6).not());
+        assert!(binary_search([-5, -4, -3, -2, -1], -6).not());
     }
 
     #[test]
     fn large_case() {
         let array: Vec<i32> = (-100..1000).collect();
-        assert!(binary_search(&array.clone(), -100));
-        assert!(binary_search(&array.clone(), 574));
-        assert!(binary_search(&array.clone(), 999));
-        assert!(binary_search(&array.clone(), 1000).not());
-        assert!(binary_search(&array.clone(), -222).not());
+        assert!(binary_search(array.clone(), -100));
+        assert!(binary_search(array.clone(), 574));
+        assert!(binary_search(array.clone(), 999));
+        assert!(binary_search(array.clone(), 1000).not());
+        assert!(binary_search(array.clone(), -222).not());
     }
 }
--- src/matrix_is_toeplitz.rs
+++ src/matrix_is_toeplitz.rs
@@ -1,18 +1,10 @@
 #[allow(dead_code)]
 pub fn is_toeplitz(matrix: Vec<Vec<i32>>) -> bool {
-    for (x, row) in matrix.iter().enumerate() {
-        if let Some(next_row) = matrix.get(x + 1) {
-            for (y, element) in row.iter().enumerate() {
-                if let Some(next_element) = next_row.get(y + 1) {
-                    if *element != *next_element {
-                        return false;
-                    }
-                } else {
-                    break;
-                };
+    for (x, row) in matrix.iter().enumerate().take(matrix.len() - 1) {
+        for (y, element) in row.iter().enumerate().take(matrix.len() - 1) {
+            if *element != matrix[x + 1][y + 1] {
+                return false;
             }
-        } else {
-            break;
         }
     }
 
@@ -24,39 +16,22 @@ pub fn is_toeplitz_bi(matrix: Vec<Vec<i32>>) -> bool {
     let mut right: bool = true;
     let mut left: bool = true;
 
-    for (x, row) in matrix.iter().enumerate() {
-        if let Some(next_row) = matrix.get(x + 1) {
-            for (y, element) in row.iter().enumerate() {
-                if let Some(next_element) = next_row.get(y + 1) {
-                    if *element != *next_element {
-                        right = false;
-                        break;
-                    }
-                } else {
-                    break;
-                };
+    for (x, row) in matrix.iter().enumerate().take(matrix.len() - 1) {
+        for (y, element) in row.iter().enumerate().take(matrix.len() - 1) {
+            if *element != matrix[x + 1][y + 1] {
+                right = false;
+                break;
             }
-        } else {
-            break;
         }
     }
 
-    for (x, row) in matrix.iter().enumerate() {
-        if let Some(next_row) = matrix.get(x + 1) {
-            for (y, element) in row.iter().enumerate() {
-                if y == 0 { continue };
-                if let Some(next_element) = next_row.get(y - 1) {
-                    if *element != *next_element {
-                        left = false;
-                        break;
-                    }
-                } else {
-                    break;
-                };
+    for (x, row) in matrix.iter().enumerate().take(matrix.len() - 1) {
+        for (y, element) in row.iter().enumerate().skip(1) {
+            if *element != matrix[x + 1][y - 1] {
+                left = false;
+                break;
             }
-        } else {
-            break;
-        };
+        }
     }
 
     right || left
@@ -141,4 +116,20 @@ mod tests {
 //             }
 //         }
 //     }
-
+//
+//     for (x, row) in matrix.iter().enumerate() {
+//         if let Some(next_row) = matrix.get(x + 1) {
+//             for (y, element) in row.iter().enumerate() {
+//                 if let Some(next_element) = next_row.get(y + 1) {
+//                     if *element != *next_element {
+//                         right = false;
+//                         break;
+//                     }
+//                 } else {
+//                     break;
+//                 };
+//             }
+//         } else {
+//             break;
+//         }
+//     }
